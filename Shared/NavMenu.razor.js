bb5c38b0b11448a4f048272eaa5b180d2c75c0c0
dady8889 <dady8889@gmail.com>
2022-12-21 18:11:54 +0100

    feat: navbar

---
+++ Shared/NavMenu.razor.js
@@ -0,0 +1,18 @@
+export function animateActiveNav(nav) {
+    const elems = document.getElementsByClassName("active");
+    if (elems.length < 1)
+        return;
+    const activeButton = elems[0].parentElement;
+    const allButtons = document.getElementsByClassName("nav-item");
+    let activeButtonIndex = 0;
+    for (let i = 0; i < allButtons.length; i++) {
+        if (activeButton == allButtons[i]) {
+            break;
+        }
+        activeButtonIndex++;
+    }
+    const shift = 2 * activeButton.clientWidth * activeButtonIndex;
+    nav.style.paddingLeft = `${nav.children[0].clientWidth / 2}px`;
+    nav.style.paddingRight = `${shift}px`;
+}
+//# sourceMappingURL=NavMenu.razor.js.map
---
+++ Shared/NavMenu.razor.ts
@@ -0,0 +1,24 @@
+﻿export function animateActiveNav(nav: HTMLElement) {
+    const elems = document.getElementsByClassName("active");
+
+    if (elems.length < 1)
+        return;
+
+    const activeButton = elems[0].parentElement;
+    const allButtons = document.getElementsByClassName("nav-item");
+    let activeButtonIndex = 0;
+
+    for (let i = 0; i < allButtons.length; i++) {
+        if (activeButton == allButtons[i]) {
+            break;
+        }
+
+        activeButtonIndex++;
+    }
+
+    const shift = 2 * activeButton.clientWidth * activeButtonIndex;
+
+    nav.style.paddingLeft = `${nav.children[0].clientWidth / 2}px`;
+
+    nav.style.paddingRight = `${shift}px`;
+}

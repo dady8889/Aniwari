cfc5b43ee0247739a999e5fab269ab46c7faedf6
dady8889 <dady8889@gmail.com>
2022-12-25 20:43:16 +0100

    chore: reusable component bases

---
+++ Shared/DisposableComponentBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Aniwari.Shared;
+
+public class DisposableComponentBase : ComponentBase, IDisposable
+{
+    protected bool Disposed { get; private set; }
+    protected CancellationTokenSource CancellationTokenSource { get; init; }
+
+    public DisposableComponentBase() 
+    {
+        CancellationTokenSource = new();
+        Disposed = false;
+    }
+
+    public virtual void Dispose()
+    {
+        Disposed = true;
+        CancellationTokenSource.Cancel();
+        CancellationTokenSource.Dispose();
+    }
+}
---
+++ Shared/ReactiveComponentBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Aniwari.BL.Services;
+
+namespace Aniwari.Shared;
+
+public class ReactiveComponentBase : DisposableComponentBase
+{
+#nullable disable
+    [Inject]
+    protected IMessageBusService MessageBusService { get; private set; }
+#nullable restore
+
+    protected List<ISubscription> Subscriptions { get; init; }
+
+    public ReactiveComponentBase()
+    {
+        Subscriptions = new();
+    }
+
+    public void ReactTo<T>(Action<T> handler) where T : IMessage
+    {
+        var sub = MessageBusService.Subscribe(handler);
+        Subscriptions.Add(sub);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+
+        foreach (var subscription in Subscriptions)
+        {
+            MessageBusService.Unsubscribe(subscription);
+        }
+
+        Subscriptions.Clear();
+    }
+}

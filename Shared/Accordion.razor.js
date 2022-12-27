9020566bdbf75da7e5401ed8c74874e0d1fe4879
dady8889 <dady8889@gmail.com>
2022-12-27 04:02:20 +0100

    feat: accordion component

---
+++ Shared/Accordion.razor.js
@@ -0,0 +1,9 @@
+export function toggleDetails(elem, expanded) {
+    if (expanded) {
+        elem.style.maxHeight = elem.scrollHeight + "px";
+    }
+    else {
+        elem.style.maxHeight = "0";
+    }
+}
+//# sourceMappingURL=Accordion.razor.js.map
---
+++ Shared/Accordion.razor.ts
@@ -0,0 +1,8 @@
+﻿export function toggleDetails(elem: HTMLElement, expanded: boolean) {
+
+    if (expanded) {
+        elem.style.maxHeight = elem.scrollHeight + "px";
+    } else {
+        elem.style.maxHeight = "0";
+    }
+}

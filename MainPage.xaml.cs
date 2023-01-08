884003174e3550eb14b6bd099c6e3f31d1a71d88
dady8889 <dady8889@gmail.com>
2023-01-08 22:45:24 +0100

    cosmetic: ui/ux

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Maui.Platform;
-using Microsoft.UI.Windowing;
-
-namespace Aniwari;
+﻿namespace Aniwari;
 
 public partial class MainPage : ContentPage
 {
@@ -9,37 +6,4 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
-
-#if WINDOWS
-    // https://github.com/MicrosoftEdge/WebView2Feedback/issues/2290
-
-    bool _foundWindow;
-
-    protected override void OnHandlerChanged()
-    {
-        base.OnHandlerChanged();
-
-        if (!_foundWindow)
-        {
-            var window = GetParentWindow();
-
-            if ((window?.Handler?.PlatformView as MauiWinUIWindow)?.GetAppWindow() is AppWindow appWindow)
-            {
-                appWindow.Changed += AppWindow_Changed;
-                _foundWindow = true;
-            }
-        }
-    }
-
-    private void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
-    {
-        /*if (args.DidPositionChange)
-        {
-            var width = this.Window.Width;
-
-            this.Window.Width = width + 1;
-            this.Window.Width = width;
-        }*/
-    }
-#endif
 }
